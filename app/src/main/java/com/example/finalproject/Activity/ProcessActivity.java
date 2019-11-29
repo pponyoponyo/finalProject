a68ec8742bec15f9faa9771b8e78efc5ead2838a
Karen <1yw@naver.com>
2019-11-29 17:05:24 +0900

    코드 정리 02

--- app/src/main/java/com/example/finalproject/Activity/ProcessActivity.java
+++ app/src/main/java/com/example/finalproject/Activity/ProcessActivity.java
@@ -2,15 +2,23 @@ package com.example.finalproject.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.finalproject.R;
 
 public class ProcessActivity extends AppCompatActivity {
 
+    private String inputUrl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_process);
+
+        Intent i = getIntent();
+        inputUrl = i.getExtras().getString("url");
+
+
     }
 }
